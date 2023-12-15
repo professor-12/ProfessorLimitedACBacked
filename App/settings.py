4d67f37f30187391a939a9bf93ef0af0d22bfca7
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2023-12-15 21:53:58 +0100

    Added enviroment variables

--- App/settings.py
+++ App/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n*vidf52)lw(1wq77hgw*pe)3%x!70l&v$&@+%8#my^@boq(r-'
+SECRET_KEY = os.environ.get("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
@@ -92,7 +92,10 @@ DATABASES = {
 }
 
 
-DATABASES["default"] = dj_database_url.parse("postgres://acm_user:sjclWGxSZFVG09vuepOwDu1RYCiDfVXd@dpg-clav6pmg1b2c73a8lmh0-a.oregon-postgres.render.com/acm")
+
+DATABASES_URL = os.environ.get('DATABASE_URL')
+
+DATABASES["default"] = dj_database_url.parse(DATABASES_URL)
 
 
 
--- App/urls.py
+++ App/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("Project.urls"))
-]+static("media/",document_root=settings.MEDIA_ROOT)
+]+staticfiles_urlpatterns()
